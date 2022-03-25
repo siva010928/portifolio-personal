9315136855872283beaf319082588b5d44fa2abf
siva010928 <siva010928@gmail.com>
2022-03-26 01:05:02 +0530

    upto date

---
+++ data/profile-siva.js
@@ -0,0 +1,144 @@
+const profileData = {
+  title: 'Profile',
+  name: 'Siva Prakash',
+  sub_title: 'Software Developer',
+  about: {
+    intro: `Hello! I am Siva Prakash.I'm focused, dedicated, hard-working and willing to learn in a changing and challenging environment.`,
+    contact: {
+      email: 'siva010928@gmail.com',
+      phone: '+91-9344015965',
+      address: 'New York, USA'
+    }
+  },
+  links: [
+    { title: 'Leetcode', src: 'https://leetcode.com/siva010928' },
+    { title: 'GitHub', src: 'https://github.com/siva010928' },
+    { title: 'CodeChef', src: 'https://codechef.com/users/siva010928'}
+  ],
+  education: [
+    {
+      alma: 'Kumaraguru College of Technology, Coimbatore',
+      duration: '2019 - 2023',
+      std: 'B.Tech, Computer Science and Engineering',
+      score: '92.5%'
+    },
+    {
+      alma: 'SSM Matriculation Higher Secondary School,Namakkal.',
+      duration: '2018 - 2019',
+      std: 'XII',
+      score: '89.6%'
+    },
+    {
+      alma: 'SSM Matriculation Higher Secondary School,Namakkal.',
+      duration: '2017 - 2018',
+      std: 'X',
+      score: '97.6%'
+    }
+  ],
+  skills: [
+    {
+      category: 'Languages',
+      topics: ['C', 'C++', 'Java', 'SQL','NoSQL']
+    },
+    {
+      category: 'Platforms',
+      topics: [
+        'Windows',' Linux (Ubuntu)'
+      ]
+    },
+    {
+      category: 'Tools',
+      topics: ['Visual Studio Code', 'Android Studio','MySql workbench','Mongodb Atlas']
+    }
+  ],
+  projects: [
+    {
+      title: 'ONLINE JOB PORTAL SYSTEM',
+      duration: 'Mar - Nov 2022',
+      link: 'https://bit.ly/si-01',
+      desc: `An interactive application that acts as a bridge
+      between a job seeker and a job recruiter who works for the company. Here job
+      seekers can search, filter, view the job, and apply for a job. Also, they can update
+      their profile and post a review for a company they work for. Recruiters can post,
+      search, filter, edit a job, join a company as a recruiter, search, filter, and review
+      candidates to update their applications.`
+    },
+    {
+      title: 'VOICE PRESCRIPTION',
+      duration: 'Apr - Jul 2020',
+      link: 'https://bit.ly/si-02',
+      desc: `A problem statement of SIH (2020) Technology Bucket: Health
+      Care and Bio-Medical Devices Category: Problem Code: AP63`
+    },
+    {
+      title: 'POPULATION GRAPH',
+      duration: 'Apr - Jul 2021',
+      link: 'https://bit.ly/sii-03',
+      desc: `Small project done while learning MERN. A person can be able to
+      change menus of countries which he wants to know and able to touch them and know
+      which year the population gets increased by which amount all details have been
+      depicted.`
+    },
+    {
+      title: 'COVID-19-TRACKER',
+      duration: 'Apr - Jul 2021',
+      link: 'http://bit.ly/si-4',
+      desc: `Mini project did while learning MERN. A person able to change
+      the menu of countries which he wants to know and able to touch the map and see the
+      conditions and size of the circle defines how many people were affected.`
+    }
+
+
+  ],
+  experiences: [
+    {
+      organization: 'World War 2',
+      title: `General`,
+      date: '1941 - 1945',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
+    },
+    {
+      organization: 'World War 1',
+      title: `Soldier`,
+      date: '1914 - 1918',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
+    }
+  ],
+  certifications: [
+    {
+      desc: `Code4All a group of young enthusiasts of UIT RGPV 24-hour Codeathon, scored 105
+      and got 1st place.`,
+      date: 'Aug 2021'
+    },
+    {
+      desc: `CodeMania A 24-hour Codeathon scored 300 marks and got 3rd place.`,
+      date: 'Jul 2021'
+    },
+    {
+      desc: `Code Drizzle 1.0, Challenge Over conducted by Developer Student Circuit KIET and got
+      9th place.`,
+      date: 'Aug 2021'
+    },
+    {
+      desc: `July Challenge 2021 Division 3 (Rated) conducted by CODECHEF. I scored 520/1000
+      and got 38th place globally.`,
+      date: 'Jul 2021'
+    },
+    {
+      desc: `August Challenge 2021 Division 3 (Rated) conducted by CODECHEF. I scored
+      620/1000 and got 210th place globally.`,
+      date: 'Aug 2021'
+    }
+  ],
+  events: []
+};
+
+
